709610e8d20ef9711d0b90945b74578a4dac6b7f
jjanddak <https://github.com/jjanddak/OWTeamSelector>
2020-04-19 19:19:54 +0900

    프레임 추가, 프레임 크기, 프레임 위치 중앙으로 수정

--- src/jjan/main/MyFrame.java
+++ src/jjan/main/MyFrame.java
@@ -15,20 +15,21 @@ public class MyFrame extends JFrame implements ActionListener{
 		super(title);//부모 생성자에 전달한다.
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		// x, y, width, height 를 한번에 지정하기 
-		setBounds(200, 200, 300, 300);
+		setBounds(200, 200, 700, 700);
 		//기본 레이아웃으로 동작하게 하기 
 		setLayout(null);
 		//화면에 보이게 하기
 		setVisible(true);
 		
 		//버튼
-		JButton btn=new JButton("눌러보셈");
+		JButton btn=new JButton("OW_버튼");
 		// x, y, width, height 를 한번에 지정
 		btn.setBounds(10, 10, 100, 40);
 		//버튼을 프레임에 추가 하기 
 		add(btn);
 		//버튼에 리스너 등록하기 
 		btn.addActionListener(this);
+		setLocationRelativeTo(null);
 	}
 	
 	//필드
